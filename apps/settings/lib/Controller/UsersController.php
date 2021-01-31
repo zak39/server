48f9734b4e5c0c8f85bc80be759b7f1f47ef7dd3
Daniel Calviño Sánchez <danxuliu@gmail.com>
2021-01-31 14:08:24 +0100

    Fix TypeError when "email" is not given in the controller request
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- apps/settings/lib/Controller/UsersController.php
+++ apps/settings/lib/Controller/UsersController.php
@@ -363,7 +363,7 @@ class UsersController extends Controller {
 									?string $twitter = null,
 									?string $twitterScope = null
 	) {
-		$email = strtolower($email);
+		$email = !is_null($email) ? strtolower($email) : $email;
 		if (!empty($email) && !$this->mailer->validateMailAddress($email)) {
 			return new DataResponse(
 				[
